0ae82ae4a5de039225eb1c491a7c1270ad478b85
= <hornet.dev.0802@gmail.com>
2023-08-31 10:07:16 -0400

    # reverse the readme file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+/*!
+ * Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
 window.addEventListener("DOMContentLoaded", (event) => {
   // Navbar shrink function
   var navbarShrink = function () {
